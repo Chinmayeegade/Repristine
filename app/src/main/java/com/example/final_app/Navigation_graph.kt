ca5d46e317a6202617fa20af090416e5783fce3d
Chinmayeegade <chinmayeepgade@gmail.com>
2022-01-02 15:04:03 +0530

    Created

---
+++ app/src/main/java/com/example/final_app/Navigation_graph.kt
@@ -0,0 +1,11 @@
+package com.example.final_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Navigation_graph : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_graph)
+    }
+}
